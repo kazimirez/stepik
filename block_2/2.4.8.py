6d1d104ff2476d98a0e64994faee7dcb32ba6fea
kazimirez <kazimirpwnz@gmail.com>
2022-03-01 08:24:31 +0300

    some changes into 'prog' just to test

--- block_2/2.4.8.py
+++ block_2/2.4.8.py
@@ -21,6 +21,7 @@ try:
     x_element = browser.find_element_by_css_selector("#input_value")
     x = x_element.text
     y = calc(x)
+    print('z')
 
     inputfield = browser.find_element_by_css_selector("#answer") 
     inputfield.send_keys(y)
