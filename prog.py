84c81af3aba865f7d513de0807d76229f9a217a5
kazimirez <kazimirpwnz@gmail.com>
2022-03-09 15:01:09 +0300

    finished 3.6.8 - changed conftest.py browser => language

--- prog.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-browser = webdriver.Chrome()
-browser.execute_script("document.title='Script executing';alert('Robots at work');")
-
-print('Z')
-print('anything')
-print('anything2')
