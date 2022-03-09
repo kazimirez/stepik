f29ecd7ec3ef543ddd89453ba818e7105cf99655
kazimirez <kazimirpwnz@gmail.com>
2022-03-09 15:26:51 +0300

    finished 3.6.9 - sneaky changes!

--- homework/test_items.py
+++ homework/test_items.py
@@ -1,11 +1,12 @@
 import time
 
-from selenium.webdriver.common.by import By
-
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_items(browser):
+def test_guest_should_see_basket_button(browser):
     browser.get(link)
-    time.sleep(5)  # хватит и 5 я уверен
-    assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
+    button = browser.find_elements_by_css_selector(
+        "#add_to_basket_form > button")
+
+    time.sleep(5)
+    assert button, 'Кнопка не найдена'
